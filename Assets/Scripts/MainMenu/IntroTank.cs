b7b0200a8a31d7c782eeb2c61cb35e17cb687db4
fmendev <36824562+fmendev@users.noreply.github.com>
2018-11-03 00:10:23 -0500

    Main Menu WIP

---
+++ Assets/Scripts/MainMenu/IntroTank.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroTank : MonoBehaviour {
+
+    public Texture2D introTank;
+    public GameObject flashPanel;
+    public GameObject mainMenuPanel;
+
+    private Color color;
+    private bool flashing = false;
+    private bool optionsFadingIn = false;
+
+    void Start ()
+    {
+        gameObject.GetComponent<RawImage>().enabled = false;
+
+        for (int i = 0; i < mainMenuPanel.transform.childCount; i++)
+        {
+            color = new Color(255, 255, 255, 0);
+            mainMenuPanel.transform.GetChild(i).GetComponentInChildren<Text>().color = color;
+        }
+
+        color = new Color(255, 255, 255, 1);
+        StartCoroutine("IntroAnimation");
+    }
+
+    private IEnumerator IntroAnimation()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+
+        transform.GetChild(1).gameObject.SetActive(false);
+        transform.GetChild(2).gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+
+        transform.GetChild(2).gameObject.SetActive(false);
+        gameObject.GetComponent<RawImage>().enabled = true;
+
+        flashPanel.SetActive(true);
+        flashing = true;
+        flashPanel.GetComponent<RawImage>().color = color;
+    }
+
+    void Update()
+    {
+        if (flashing)
+        {
+            float alpha = flashPanel.GetComponent<RawImage>().color.a;
+            color = new Color(255, 255, 255, alpha - .01f);
+            flashPanel.GetComponent<RawImage>().color = color;
+
+            if (color.a <= 0)
+            {
+                color = new Color(255, 255, 255, 1);
+                flashPanel.GetComponent<RawImage>().color = color;
+                flashing = false;
+                flashPanel.SetActive(false);
+                optionsFadingIn = true;
+            }
+        }
+
+        if (optionsFadingIn)
+        {
+            for (int i = 0; i < mainMenuPanel.transform.childCount; i++)
+            {
+                float alpha = mainMenuPanel.transform.GetChild(i).GetComponentInChildren<Text>().color.a;
+                color = new Color(255, 255, 255, alpha + .01f);
+                mainMenuPanel.transform.GetChild(i).GetComponentInChildren<Text>().color = color;
+
+                if (color.a >= 255)
+                {
+                    color = new Color(255, 255, 255, 1);
+                    mainMenuPanel.transform.GetChild(i).GetComponentInChildren<Text>().color = color;
+                    optionsFadingIn = false;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenu/MenuSelectionSprite.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuSelectionSprite : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject selectionSprite;
+
+    private Vector3 buttonPosition;
+
+    float offsetX = -10;
+
+    void Awake()
+    {
+        buttonPosition = gameObject.transform.position;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("In!");
+        selectionSprite.gameObject.SetActive(true);
+        selectionSprite.GetComponent<RectTransform>().position = new Vector3(buttonPosition.x + offsetX, buttonPosition.y);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Out!");
+        selectionSprite.gameObject.SetActive(false);
+    }
+}
